5a79963fadfdd81ecbeb5ac873644af18b3abccc
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-08-28 04:15:17 -0400

    test: add touchpad toggle test

--- tests/driver_test.go
+++ tests/driver_test.go
@@ -110,3 +110,24 @@ func TestRapidCharge(t *testing.T) {
 
 	fmt.Printf("%v ", Success)
 }
+
+func TestTouchPad(t *testing.T) {
+	driver := drivers.TouchPad
+
+	if driver.GetStatus(&driver) == "" {
+		t.Errorf("%v failed to retrieve current %v status", Fail, driver.File)
+		return
+	}
+	fmt.Printf("Current %v Status: %v\n", driver.File, driver.GetStatus(&driver))
+
+	fmt.Printf("Toggling %v\n", driver.File)
+	driver.ToggleDriverMode()
+
+	if driver.GetStatus(&driver) == "" {
+		t.Errorf("%v failed to retrieve current %v status", Fail, driver.File)
+		return
+	}
+	fmt.Printf("Current %v Status: %v\n", driver.File, driver.GetStatus(&driver))
+
+	fmt.Printf("%v ", Success)
+}
