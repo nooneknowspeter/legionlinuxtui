757b04b1ceb6eac2af975fcb34caa536dd62f086
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-08-28 04:15:31 -0400

    test: add usb charging toggle test

--- tests/driver_test.go
+++ tests/driver_test.go
@@ -131,3 +131,24 @@ func TestTouchPad(t *testing.T) {
 
 	fmt.Printf("%v ", Success)
 }
+
+func TestUSBCharging(t *testing.T) {
+	driver := drivers.USBCharging
+
+	if driver.GetStatus(&driver) == "" {
+		t.Errorf("%v failed to retrieve current %v status", Fail, driver.File)
+		return
+	}
+	fmt.Printf("Current %v Status: %v\n", driver.File, driver.GetStatus(&driver))
+
+	fmt.Printf("Toggling %v\n", driver.File)
+	driver.ToggleDriverMode()
+
+	if driver.GetStatus(&driver) == "" {
+		t.Errorf("%v failed to retrieve current %v status", Fail, driver.File)
+		return
+	}
+	fmt.Printf("Current %v Status: %v\n", driver.File, driver.GetStatus(&driver))
+
+	fmt.Printf("%v ", Success)
+}
