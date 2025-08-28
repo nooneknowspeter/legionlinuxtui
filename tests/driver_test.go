c43129e7e28164908b85a55f7a1f45c281a28af3
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-08-28 04:14:29 -0400

    test: add fnlock toggle test

--- tests/driver_test.go
+++ tests/driver_test.go
@@ -47,3 +47,24 @@ func TestConservationMode(t *testing.T) {
 
 	fmt.Printf("%v ", Success)
 }
+
+func TestFNLock(t *testing.T) {
+	driver := drivers.FNLock
+
+	if driver.GetStatus(&driver) == "" {
+		t.Errorf("%v failed to retrieve current %v status", Fail, driver.File)
+		return
+	}
+	fmt.Printf("Current %v Status: %v\n", driver.File, driver.GetStatus(&driver))
+
+	fmt.Printf("Toggling %v\n", driver.File)
+	driver.ToggleDriverMode()
+
+	if driver.GetStatus(&driver) == "" {
+		t.Errorf("%v failed to retrieve current %v status", Fail, driver.File)
+		return
+	}
+	fmt.Printf("Current %v Status: %v\n", driver.File, driver.GetStatus(&driver))
+
+	fmt.Printf("%v ", Success)
+}
