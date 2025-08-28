325de19e0e6460a005e03eb549d88a7fcfa33ac5
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-08-28 04:17:00 -0400

    test: add rw file test

---
+++ tests/io_test.go
@@ -0,0 +1,40 @@
+package tests
+
+import (
+	"fmt"
+	"legionlinuxtui/src/helpers"
+	"testing"
+)
+
+var (
+	testFile string = "test_file"
+)
+
+func TestRW(t *testing.T) {
+	var expectedContents string = ""
+	helpers.WriteToFile(testFile, expectedContents) // clear file
+
+	var file string = helpers.ReadFile(testFile)
+
+	fmt.Printf("File Contents: %v\n", file)
+	fmt.Printf("Expected Contents: %v\n", expectedContents)
+	if file != expectedContents {
+		t.Errorf("%v contents do not match what was expected in the file", Fail)
+		return
+	}
+
+	expectedContents = "just another fucking test"
+	helpers.WriteToFile(testFile, expectedContents)
+
+	file = helpers.ReadFile(testFile)
+
+	fmt.Printf("File Contents: %v\n", file)
+	fmt.Printf("Expected Contents: %v\n", expectedContents)
+
+	if file != expectedContents {
+		t.Errorf("%v contents do not match what was expected in the file", Fail)
+		return
+	}
+
+	fmt.Printf("%v contents match ", Success)
+}
