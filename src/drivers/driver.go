bcf49261471d42731f42bb2cdf5cd5ab10c11e19
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-08-28 03:48:39 -0400

    feat: implement methods to cycle through driver modes

--- src/drivers/driver.go
+++ src/drivers/driver.go
@@ -1,6 +1,7 @@
 package drivers
 
 import (
+	"fmt"
 	"legionlinuxtui/src/helpers"
 	"strconv"
 )
@@ -26,4 +27,26 @@ func (s *DriverModuleFunction) readValue() int {
 
 func (s *DriverModuleFunction) writeValue(value string) {
 	helpers.WriteToFile(s.SysFSLocation+s.File, value)
+}
+
+// function implementation of iterator style pattern
+// loop through modes in cycle; next
+// currentMode value must be indexed at 0
+func (s *DriverModuleFunction) toggleIterator(currentMode int, numberOfModes int) int {
+	return ((1 + currentMode) % numberOfModes)
+}
+
+func (s *DriverModuleFunction) ToggleDriverMode() {
+	currentMode := s.readValue()
+	numberOfModes := len(s.DriverModes)
+	var nextMode int
+
+	if numberOfModes > 2 {
+		nextMode = s.toggleIterator(currentMode-1, numberOfModes)
+	} else {
+		nextMode = s.toggleIterator(currentMode, numberOfModes)
+	}
+
+	s.writeValue(fmt.Sprintf("%v\n", nextMode))
+	fmt.Printf("toggled %v: %v -> %v\n", s.File, s.DriverModes[currentMode], s.DriverModes[s.readValue()])
 }
