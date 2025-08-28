f5e94d477eba8e7b719f759e802eaf1743bfaec5
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-08-28 03:33:55 -0400

    feat: cpu information
    - model
    - temperature

--- src/system/information.go
+++ src/system/information.go
@@ -1,7 +1,10 @@
 package system
 
 import (
+	"encoding/json"
+	"fmt"
 	"legionlinuxtui/src/helpers"
+	"os/exec"
 )
 
 type (
@@ -11,6 +14,11 @@ type (
 		Family      string
 		Vendor      string
 	}
+
+	cpuSensorInformation struct {
+		Name        func() string
+		Temperature func() string
+	}
 )
 
 var (
@@ -19,5 +27,31 @@ var (
 		BiosVersion: helpers.ReadFile(DEVICEINFORMATIONPATH + "bios_version"),
 		Family:      helpers.ReadFile(DEVICEINFORMATIONPATH + "product_family"),
 		Vendor:      helpers.ReadFile(DEVICEINFORMATIONPATH + "sys_vendor"),
+	}
+
+	CPU cpuSensorInformation = cpuSensorInformation{
+		Name: func() string {
+			stdout, err := exec.Command("lscpu", "-e=ModelName", "-J").Output()
+			if err != nil {
+				fmt.Printf("command lscpu not found %v", err)
+			}
+
+			var data map[string]any
+			if err := json.Unmarshal(stdout, &data); err != nil {
+				panic(err)
+			}
+
+			cpuModel := fmt.Sprintf("%v", data["cpus"].([]any)[0].(map[string]any)["modelname"].(string))
+
+			return cpuModel
+		},
+
+		Temperature: func() string {
+			file := helpers.ReadFile(CPUINFORMATIONPATH + "temp1_input")
+
+			cpuTemperature := fmt.Sprintf("%v", string(file[0:2]))
+
+			return cpuTemperature
+		},
 	}
 )
