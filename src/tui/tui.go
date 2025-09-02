91dc581f6a5042c4d59da3f56385585c46568be4
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-02 18:03:52 -0400

    feat: setup bubbletea fullscreen tui canvas

---
+++ src/tui/tui.go
@@ -0,0 +1,56 @@
+package tui
+
+import (
+	"log"
+	"time"
+
+	"legionlinuxtui/src/tui/panes"
+	"legionlinuxtui/src/tui/styles"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type (
+	model struct {
+		terminalWidth  int
+		terminalHeight int
+		windowTitle    string
+	}
+)
+
+func initModel() model {
+	return model{
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return tea.Batch(
+		tea.SetWindowTitle("legionlinuxtui"),
+	)
+}
+
+func (m model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := message.(type) {
+	case tea.WindowSizeMsg:
+		m.terminalHeight = msg.Height
+		m.terminalWidth = msg.Width
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		}
+
+	return m, nil
+}
+
+func (m model) View() string {
+
+}
+
+func Run() {
+	p := tea.NewProgram(initModel(), tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
