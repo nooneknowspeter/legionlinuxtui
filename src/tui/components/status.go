9cae87cf9fa7e6ac6d80054b7b37b708c5a34ad6
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-02 17:56:44 -0400

    feat: add status component

---
+++ src/tui/components/status.go
@@ -0,0 +1,19 @@
+package components
+
+import (
+	"github.com/charmbracelet/lipgloss"
+)
+
+func StatusComponent(title string, information ...string) string {
+	title = lipgloss.NewStyle().Bold(true).Render(title)
+
+	var componentContent []string = []string{}
+
+	componentContent = append(componentContent, title)
+
+	for _, v := range information {
+		componentContent = append(componentContent, v)
+	}
+
+	return lipgloss.NewStyle().Width(30).Render(lipgloss.JoinVertical(lipgloss.Top, componentContent...))
+}
