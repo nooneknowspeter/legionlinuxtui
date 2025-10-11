59cff3bc66ce3ec1dc8729f8f446d7cc44badb55
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-10-11 02:34:15 -0400

    refactor: add comments to driver sys path constants

--- src/system/paths.go
+++ src/system/paths.go
@@ -2,9 +2,12 @@ package system
 
 const (
 	BATTERYINFORMATIONPATH string = "/sys/class/power_supply/BAT0/"
-	CPUINFORMATIONPATH     string = "/sys/class/hwmon/hwmon5/"
-	FANINFORMATIONPATH     string = "/sys/class/hwmon/hwmon3/"
-	DEVICEINFORMATIONPATH  string = "/sys/devices/virtual/dmi/id/"
-	IDEASYSTEMDRIVERPATH   string = "/sys/module/legion_laptop/drivers/platform:legion/PNP0C09:00/VPC2004:00/"
+	// FIX: use another dir instead of hwmon or find paths at runtime; hwmon allocates dirs dynamically
+	CPUINFORMATIONPATH    string = "/sys/class/hwmon/hwmon5/"
+	FANINFORMATIONPATH    string = "/sys/class/hwmon/hwmon3/"
+	DEVICEINFORMATIONPATH string = "/sys/devices/virtual/dmi/id/"
+	// IDEASYSTEMDRIVERPATH -> sysfs ideapad module drivers
+	IDEASYSTEMDRIVERPATH string = "/sys/module/legion_laptop/drivers/platform:legion/PNP0C09:00/VPC2004:00/"
+	// LEGIONSYSTEMDRIVERPATH -> sysfs legion linux module drivers
 	LEGIONSYSTEMDRIVERPATH string = "/sys/module/legion_laptop/drivers/platform:legion/PNP0C09:00/"
 )
