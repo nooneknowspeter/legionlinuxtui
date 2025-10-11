f5e0444a8f10270df5aaf8fda7a471c5b5cf4734
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-10-11 01:44:41 -0400

    feat: update global styles

--- src/tui/styles/styles.go
+++ src/tui/styles/styles.go
@@ -1,11 +1,18 @@
 package styles
 
 import (
+	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/lipgloss"
 )
 
 var (
-	Heading     = lipgloss.NewStyle().Bold(true)
-	Padding int = 1
-	Border      = lipgloss.NewStyle().Border(lipgloss.NormalBorder(), true).Padding(Padding)
+	Heading               = lipgloss.NewStyle().Bold(true)
+	Padding           int = 4
+	Border                = lipgloss.NewStyle().Border(lipgloss.NormalBorder(), true).Padding(Padding)
+	TitleStyle            = lipgloss.NewStyle().MarginLeft(2).Bold(true)
+	ItemStyle             = lipgloss.NewStyle().PaddingLeft(4)
+	SelectedItemStyle     = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("#666666"))
+	PaginationStyle       = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	HelpStyle             = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	QuitTextStyle         = lipgloss.NewStyle().Margin(1, 0, 2, 4)
 )
