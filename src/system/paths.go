6e3942d61129b958cb3d6fe166fe21db3ec94549
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-10-15 20:16:08 -0400

    refactor: update sensor information paths

--- src/system/paths.go
+++ src/system/paths.go
@@ -2,10 +2,8 @@ package system
 
 const (
 	BATTERYINFORMATIONPATH string = "/sys/class/power_supply/BAT0/"
-	// FIX: use another dir instead of hwmon or find paths at runtime; hwmon allocates dirs dynamically
-	CPUINFORMATIONPATH    string = "/sys/class/hwmon/hwmon5/"
-	FANINFORMATIONPATH    string = "/sys/class/hwmon/hwmon3/"
-	DEVICEINFORMATIONPATH string = "/sys/devices/virtual/dmi/id/"
+	SENSORINFORMATIONPATH  string = "/sys/module/legion_laptop/drivers/platform:legion/PNP0C09:00/hwmon/hwmon3/"
+	DEVICEINFORMATIONPATH  string = "/sys/devices/virtual/dmi/id/"
 	// IDEASYSTEMDRIVERPATH -> sysfs ideapad module drivers
 	IDEASYSTEMDRIVERPATH string = "/sys/module/legion_laptop/drivers/platform:legion/PNP0C09:00/VPC2004:00/"
 	// LEGIONSYSTEMDRIVERPATH -> sysfs legion linux module drivers
